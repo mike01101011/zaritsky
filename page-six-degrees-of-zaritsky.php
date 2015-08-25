53e75f25f1257f1b0f72170cdebd8927004ba5d1
Nikin Nagewadia <nikin.n@gmail.com>
2015-08-25 00:11:20 -0400

    include images

--- page-six-degrees-of-zaritsky.php
+++ page-six-degrees-of-zaritsky.php
@@ -9,31 +9,26 @@
 	<div class="degree-option">
 		<div class="row">
 			<div class="degree-1">
-				<h1>test 1</h1>
+				
 			</div>
 
 			<div class="degree-2">
-				<h1>test 2</h1>
 			</div>
 		</div>
 		
 		<div class="row">
 			<div class="degree-3">
-				<h1>test 3</h1>
 			</div>
 
 			<div class="degree-4">
-				<h1>test 4</h1>
 			</div>
 		</div>
 		
 		<div class="row">
 			<div class="degree-5">
-				<h1>test 5</h1>
 			</div>
 
 			<div class="degree-6">
-				<h1>test 6</h1>
 			</div>
 		</div>
 	</div>
