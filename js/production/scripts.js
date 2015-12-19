b4f46f573d1bea59ac55a035c04eb774b157c3a4
Nikin Nagewadia <nikin.n@gmail.com>
2015-12-19 16:47:49 -0500

    use namespace model and implement svg injection

--- js/production/scripts.js
+++ js/production/scripts.js
@@ -1,36 +1,48 @@
-$(function(){
-	// Smooth Scroll
-	$(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
-	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-	      var target = $(this.hash);
-	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-	      if (target.length) {
-	        $('html,body').animate({
-	          scrollTop: target.offset().top - 45
-	        }, 1000);
-	        return false;
-	      }
-	    }
-	  });
-	});
+var zaritskyApp = {};
 
-	//When click menu button, show menu
+// Smooth Scroll
+zaritskyApp.smoothScroll = function() {
+	$('a[href*=#]:not([href=#])').click(function() {
+	  if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	    var target = $(this.hash);
+	    target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	    if (target.length) {
+	      $('html,body').animate({
+	        scrollTop: target.offset().top - 45
+	      }, 1000);
+	      return false;
+	    }
+	  }
+	});
+};
+
+
+//When click menu button, show menu
+zaritskyApp.showMenu = function() {
 	$('#menu-icon-button').click(function() {
 		$('nav').addClass('open');
 		$('body').css('overflow','hidden');
 	});
+};
 
-	//When click on exit button, close menu button
+
+//When click on exit button, close menu button
+zaritskyApp.closeMenu = function() {
 	$('.exit-button').click(function() {
 		$('nav').removeClass('open');
 		$('body').css('overflow','visible');
-	});
+	});	
+};
 
-	// Used to ensure all videos are responsive
+
+// FitVid Plugin
+zaritskyApp.fitVid = function() {
 	$('.container').fitVids();
+};
 
 
+// Flickity used in 6 Degrees
+zaritskyApp.flickity = function() {
 	$(".main-gallery").flickity({
 	  // options
 		cellAlign: "left",
@@ -46,8 +58,28 @@ $(function(){
 		watchCSS: true,
 		imagesLoaded: true
 	});
+};
 
+
+// Indentation of quotes
+zaritskyApp.exdent = function() {
 	$('blockquote, q').exdent({
 		by: '.425em'
-    });
+	});
+};
+
+
+// Load on Page Load
+zaritskyApp.init = function() {
+	zaritskyApp.smoothScroll();
+	zaritskyApp.showMenu();
+	zaritskyApp.closeMenu();
+	zaritskyApp.fitVid();
+	zaritskyApp.flickity();
+	zaritskyApp.exdent();
+};
+
+
+$(function(){
+	zaritskyApp.init();
 });
