f1fc8e438d1a03d242aa1cdb36a7fbe2ab76f268
Nikin Nagewadia <nikin.n@gmail.com>
2016-01-10 14:36:25 -0500

    revisions with Jenn

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 <main>
 	<div class="hero">
 		<div class="container vertical-align">
-			<h1>Who in the World<br>is John Zaritsky?</h1>
+			<h1>Mr Zaritsky<br><span class="on_tv">on <em>TV</em></span></h1>
 		</div> <!-- /.container -->
 		<div class="container">
 			<a href="#below"><span class="arrow-down-button"></span></a>
