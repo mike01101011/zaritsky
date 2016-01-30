1dde67e65d5d500cd92f684fe17b230d5df6c646
Nikin Nagewadia <nikin.n@gmail.com>
2016-01-30 16:18:12 -0500

    added google analytics and changes as per jen's request

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
 		  <div class="container">
 			<div class="produced-by">
 				<div class="big-twin-production">
-					Big Twin Production's &copy; <?php echo date("Y") ?>
+					Big Twin Productions &copy; <?php echo date("Y") ?>
 					<a href="http://www.bigtwinproductions.com/" target="_blank"><span class="big-twin-logo"></span></a>
 				</div>
 
@@ -25,11 +25,14 @@
 		<script src="<?php bloginfo('template_url'); ?>/js/build.js"></script>
 
 		<script>
-		/* Google Analytics! */
-		 var _gaq=[["_setAccount","UA-XXXXX-X"],["_trackPageview"]]; // Change UA-XXXXX-X to be your site's ID
-		 (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];g.async=1;
-		 g.src=("https:"==location.protocol?"//ssl":"//www")+".google-analytics.com/ga.js";
-		 s.parentNode.insertBefore(g,s)}(document,"script"));
+			/* Google Analytics! */
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+			ga('create', 'UA-73058902-1', 'auto');
+			ga('send', 'pageview');
 		</script>
 
 
