5409717636cf7e1cd172c68da0b34ff20388792b
Nikin Nagewadia <nikin.n@gmail.com>
2016-02-11 23:38:42 -0500

    update google analytics

--- footer.php
+++ footer.php
@@ -31,10 +31,8 @@
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			ga('create', 'UA-73058902-1', 'auto');
+			ga('create', 'UA-73696813-1', 'auto');
 			ga('send', 'pageview');
 		</script>
-
-
 	</body>
 	</html>
